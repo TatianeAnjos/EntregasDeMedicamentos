f167bafcd8b666a0f353552577256e2afd9a3d0d
Tatiane Anjos <tatianeanjos1997@gmail.com>
2021-07-04 12:23:15 -0300

    Projeto criado

---
+++ src/main/java/com/tatiane/EntregaMedicamentosSus/EntregaMedicamentosSusApplication.java
@@ -0,0 +1,13 @@
+package com.tatiane.EntregaMedicamentosSus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaMedicamentosSusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaMedicamentosSusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatiane/EntregaMedicamentosSus/EntregaMedicamentosSusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatiane.EntregaMedicamentosSus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaMedicamentosSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
